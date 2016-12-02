643c5535551d5362b6945523de2af93f3d6cdaaf
Kirill Kudaev <kudaev.k@gmail.com>
2016-12-02 11:02:01 +0200

    Level 1 completed. upd 1 uncomented resource deploy

--- cookbooks/testcook/recipes/default.rb
+++ cookbooks/testcook/recipes/default.rb
@@ -52,7 +52,7 @@ service 'mariadb' do
     action [:enable, :start]
 end
 
-#create an empty database for wordpress app and grant all perms on it
+#create an empty database for wordpress app and grant all privs on it
 #to wordpress user
 execute 'db-initial' do
   command 'mysql -e \'create database wordpress;\' && mysql -e \'GRANT ALL PRIVILEGES ON wordpress.* TO "wpuser"@"localhost" IDENTIFIED BY "wppass";\' && mysql -e \'FLUSH PRIVILEGES;\''
@@ -61,62 +61,65 @@ end
 
 
 #deploy WordPress from git using private repo (auth by key)
-#package 'git'  do
-#  action :install
-#end
 
-#directory '/tmp/.ssh' do
-#    owner 'root'
-#    group 'root'
-#    mode '0755'
-#    action :create
-#end
+# install git if not already installed
+package 'git'  do
+  action :install
+end
 
-#directory '/var/www/wordpress' do
-#    owner 'root'
-#    group 'root'
-#    mode '0755'
-#    action :create
-#end
+directory '/tmp/.ssh' do
+    mode '0755'
+    action :create
+end
 
-#directory '/root/.ssh' do
-#    owner 'root'
-#    group 'root'
-#    mode '0755'
-#    action :create
-#end
+directory '/var/www/wordpress' do
+    mode '0755'
+    action :create
+end
+
+directory '/root/.ssh' do
+    mode '0755'
+    action :create
+end
 
 
-#template '/tmp/.ssh/wrap-ssh4git.sh' do
-#    source 'wrap-ssh4git.erb'
-#    owner 'root'
-#    mode '0770'
-#end
+template '/tmp/.ssh/wrap-ssh4git.sh' do
+    source 'wrap-ssh4git.erb'
+    mode '0770'
+end
 
-#template '/root/.ssh/deploy_key_pub' do
-#    source 'deploy_key_pub.erb'
-#    owner 'root'
-#    mode '0600'
-#end
+template '/root/.ssh/deploy_key_pub' do
+    source 'deploy_key_pub.erb'
+    mode '0600'
+end
 
-#template '/root/.ssh/deploy_key' do
-#    source 'deploy_key.erb'
-#    owner 'root'
-#    mode '0600'
-#end
+template '/root/.ssh/deploy_key' do
+    source 'deploy_key.erb'
+    mode '0600'
+end
 
-#deploy 'private_repo' do
-#    repo 'git@github.com/WordPress/WordPress.git'
-#    user 'nginx'
-#    deploy_to '/var/www/wordpress'
-#    ssh_wrapper '/tmp/.ssh/wrap-ssh4git.sh'
-#    action :deploy
-#end
+deploy 'private_repo' do
+    repo 'git@github.com/WordPress/WordPress.git'
+    user 'nginx'
+    deploy_to '/var/www/wordpress'
+    ssh_wrapper '/tmp/.ssh/wrap-ssh4git.sh'
+    action :deploy
+end
 
-#create wordpress config
+# delete temporary dirs
+directory '/tmp/.ssh' do
+    action :delete
+    recursive true
+end
+
+directory '/root/.ssh' do
+    action :delete
+    recursive true
+end
+
+
+#wordpress config creation
 template '/var/www/wordpress/wp-config.php' do
     source 'wp-config.erb'
     mode '0644'
-    owner 'root'
-    group 'root'
 end
